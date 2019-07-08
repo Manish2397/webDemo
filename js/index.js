93061d26dc3518df1de7e542140f116f5c5943d4
manish2397 <rawatmanish2397@gmail.com>
2019-07-08 23:44:53 +0530

    new concept learned

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log("manish");
+class Animal{
+    constructor(name,age){
+        console.log("object created!",name,age)
+    }
+}
+var dog = new Animal("puppy",23);
+
+var inputText = document.getElementsByClassName("myInput")
+console.log(inputText[0].value);
