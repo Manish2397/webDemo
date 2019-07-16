3fbef9b93d0d64abbefc5af7fcadd58d67cd265d
manish2397 <rawatmanish2397@gmail.com>
2019-07-16 11:21:18 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-class Animal{
-    constructor(name,age){
-        console.log("object created!",name,age)
-    }
-}
-var dog = new Animal("puppy",23);
-
-var inputText = document.getElementsByClassName("myInput")
-console.log(inputText[0].value);
+$(document).ready(function(){
+    $('.main-menu').on("click",function(){
+        var submenu = $('.submenu')
+        submenu.addClass='active';
+        console.log("should appear")
+    })
+    
+})
